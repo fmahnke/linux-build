27b01e96b58d72cb467f5b19001bdf19f8468fdb
Danct12 <danct12@disroot.org>
2020-09-27 13:17:18 +0700

    megapizel
    Signed-off-by: Danct12 <danct12@disroot.org>

--- make_rootfs.sh
+++ make_rootfs.sh
@@ -91,7 +91,7 @@ cp $OTHERDIR/locale.gen "$DEST/etc/locale.gen-all"
 
 mv "$DEST/etc/pacman.d/mirrorlist" "$DEST/etc/pacman.d/mirrorlist.default"
 
-echo "Server = http://sg.mirror.archlinuxarm.org/\$arch/\$repo" > "$DEST/etc/pacman.d/mirrorlist"
+echo "Server = http://tw.mirror.archlinuxarm.org/\$arch/\$repo" > "$DEST/etc/pacman.d/mirrorlist"
 
 cat > "$DEST/second-phase" <<EOF
 #!/bin/sh
@@ -104,7 +104,7 @@ pacman -S --noconfirm --overwrite=* --disable-download-timeout --needed dosfstoo
 
 pacman -S --noconfirm --overwrite=* --disable-download-timeout --needed mesa-git danctnix-phosh-ui-meta flashlight xdg-user-dirs noto-fonts-emoji
 
-pacman -S --noconfirm --overwrite=* --disable-download-timeout --needed lollypop gedit evince-mobile mobile-config-firefox gnome-calculator gnome-clocks gnome-maps pinhole gnome-usage-mobile gtherm geary-mobile purple-matrix purple-telegram
+pacman -S --noconfirm --overwrite=* --disable-download-timeout --needed lollypop gedit evince-mobile mobile-config-firefox gnome-calculator gnome-clocks gnome-maps megapixels gnome-usage-mobile gtherm geary-mobile purple-matrix purple-telegram
 
 systemctl disable sshd
 
