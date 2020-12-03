0c8b0c51b0a5a8d4424d1631c460da9620e31dae
Danct12 <danct12@disroot.org>
2020-12-03 09:34:39 +0700

    add support for multi partition

--- make_image.sh
+++ make_image.sh
@@ -22,12 +22,15 @@ LOOP_DEVICE=$(losetup -f)
 losetup -P $LOOP_DEVICE $IMAGE_NAME
 
 echo "Creating filesystems"
-mkfs.ext4 ${LOOP_DEVICE}p1
+mkfs.vfat ${LOOP_DEVICE}p1
+mkfs.ext4 ${LOOP_DEVICE}p2
 
 TEMP_ROOT=$(mktemp -d)
 mkdir -p $TEMP_ROOT
 echo "Mounting rootfs"
-mount ${LOOP_DEVICE}p1 $TEMP_ROOT
+mount ${LOOP_DEVICE}p2 $TEMP_ROOT
+mkdir -p ${TEMP_ROOT}/boot
+mount ${LOOP_DEVICE}p1 ${TEMP_ROOT}/boot
 
 echo "Unpacking rootfs archive"
 bsdtar -xpf "$TARBALL" -C "$TEMP_ROOT" || true
@@ -35,8 +38,11 @@ bsdtar -xpf "$TARBALL" -C "$TEMP_ROOT" || true
 echo "Installing bootloader"
 dd if=$TEMP_ROOT/boot/$BOOTLOADER of=${LOOP_DEVICE} bs=8k seek=1
 
+echo "Generating fstab"
+genfstab -U $TEMP_ROOT >> ${TEMP_ROOT}/etc/fstab
+
 echo "Unmounting rootfs"
-umount $TEMP_ROOT
+umount -R $TEMP_ROOT
 rm -rf $TEMP_ROOT
 
 # Detach loop device
